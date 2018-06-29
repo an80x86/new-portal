2c57bd8bdbd3b1e940301eac1a1ab272158ca18c
= <=>
2018-06-29 18:22:04 +0300

    düzenleme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import { PostList, PostEdit, PostCreate } from './posts';
+import { UserList } from './users';
+import Dashboard from './Dashboard';
+import authProvider from './authProvider';
+import dataProvider from './dataProvider';
+import Login from './login';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+//const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+
+const App = () => (
+  <Admin 
+  title="Uyum Portal v2.0"
+    dashboard={Dashboard}
+    loginPage={Login} 
+    authProvider={authProvider} 
+    dataProvider={dataProvider}>
+      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+      <Resource name="users" list={UserList} icon={UserIcon} />
+  </Admin>
+);
 
 export default App;
